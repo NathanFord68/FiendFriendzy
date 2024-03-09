e568156dd47dc8ecf41b55d37f3d6b9a4f96cd31
Nathan Ford <NathanFord68@gmail.com>
2024-03-09 17:56:14 -0500

    Added some attrubites to the melee character, TODO we need to extract attributes out into their own file

--- units/main.gd
+++ units/main.gd
@@ -1,7 +1,14 @@
-class_name Unit
+class_name UnitAttributes
 
-func _init():
-	pass
-	
-func move():
-	print("Move from units/main.gd")
+var health : int
+var move_range : int
+var attack_range : int
+
+func _init( 
+	health = 0,
+	move_range = 0, 
+	attack_range = 0 
+):
+	self.health = health
+	self.move_range = move_range
+	self.attack_range = attack_range
---
+++ units/melee/CharacterBody3D.gd
@@ -0,0 +1,6 @@
+extends CharacterBody3D
+
+# Define attributes
+@export var health : int = 100
+@export var move_range = 10
+@export var attack_range = 1
