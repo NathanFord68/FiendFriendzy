5c4b911730825cb6b80a5d87d59ee474b6f482db
Nathan Ford <NathanFord68@gmail.com>
2024-03-09 14:19:49 -0500

    Added functionality to select the unit

--- player/Character.gd
+++ player/Character.gd
@@ -1,9 +1,24 @@
 extends Node3D
 
 var direction : Vector3 = Vector3()
+var camera : Camera3D = null
+
+@export var raycast_length : float = 1000
+
+var selected_troop : CharacterBody3D = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	
+	# Get the camera
+	camera = get_node("SpringArm3D/Camera3D")
+	if camera == null:
+		push_error("Could not find camera")
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index ==1:
+		handle_mouse_click()
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -21,3 +36,24 @@ func _process(delta):
 	translate(direction)
 	
 	direction = Vector3()
+	
+# TODO add logic to handle different types of mouse clicks
+func handle_mouse_click():
+	# Get mouse position
+	var mouse_pos = get_viewport().get_mouse_position()
+	
+	# Ray cast out from mouse position
+	var f = camera.project_ray_origin(mouse_pos)
+	var t = f + camera.project_ray_normal(mouse_pos) * raycast_length
+	
+	# Detect collisions and get troops
+	var space_state = get_world_3d().direct_space_state
+	var trace = space_state.intersect_ray(PhysicsRayQueryParameters3D.create(f, t))
+	selected_troop = trace.collider
+	
+	# Short 
+	if selected_troop == null:
+		return
+	
+	print(selected_troop.position)
+	
---
+++ units/main.gd
@@ -0,0 +1,7 @@
+class_name Unit
+
+func _init():
+	pass
+	
+func move():
+	print("Move from units/main.gd")
