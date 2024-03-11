4a032d7fa1832362487b800b87ef3a6fadf88dab
Nathan Ford <NathanFord68@gmail.com>
2024-03-11 15:08:11 -0400

    Mergine attribute resource creation

---
+++ resources/troop_attributes.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+class_name TroopAttributes
+
+# Define attributes
+@export var health : int 		= 100
+@export var move_range : int 	= 11
+@export var attack_range :int 	= 1
+@export var attack_damage : int = 5
--- units/melee/CharacterBody3D.gd
+++ units/melee/CharacterBody3D.gd
@@ -1,17 +1,13 @@
 extends CharacterBody3D
 
-# Define attributes
-@export var health : int 		= 100
-@export var move_range : int 	= 11
-@export var attack_range :int 	= 1
-@export var attack_damage : int = 5
+@export var attributes : TroopAttributes = TroopAttributes.new()
 
 var can_move = true
 var can_attack = true
 
 func take_damage(attacking_troop : CharacterBody3D):
-	self.health -= attacking_troop.attack_damage
+	self.attributes.health -= attacking_troop.attack_damage
 	
-	if health <= 0:
+	if attributes.health <= 0:
 		self.queue_free()
 		print(str(self.get_rid())+ " has died")
